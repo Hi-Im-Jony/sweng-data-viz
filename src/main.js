a2541b8eb79c1c58c78379cfec65752007ae2ac6
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-03 21:28:50 +0000

    using chartjs and chartkick

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import { Chart } from "chart.js";
+import Chartkick from "vue-chartkick";
+
+Vue.use(Chartkick.use(Chart));
 
 Vue.config.productionTip = false;
 
