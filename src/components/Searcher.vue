2f7a25d617f78862c2042396fbb212e08d46717c
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-06 19:33:09 +0000

    graph parsing finally done!

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -28,9 +28,9 @@
         <h3>Percentage Of Code In "X" Language</h3>
         <pie-chart
           id="pchart"
-          :data="this.info.pChartData"
+          :data="this.info.pChartData.percentages"
           suffix="%"
-          label="Title"
+          :legend="false"
         />
       </div>
       <div id="bchart-container">
@@ -54,10 +54,11 @@ import Vue from "vue";
 export default {
   data: () => ({
     info: {
-      pChartData: null,
-      bChartData: {
-        Test: 0,
+      pChartData: {
+        metrics: {},
+        percentages: {},
       },
+      bChartData: {},
     },
     name: "",
     selection: "",
@@ -65,19 +66,19 @@ export default {
     errorMsg: "",
     header: {
       headers: {
-        Authorization: "token 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",
+        Authorization: "token <input_token>",
       },
     },
   }),
   methods: {
     callAPI: function () {
-      this.info.pChartData = null;
-      this.info.bChartData = null;
+      this.info.pChartData = { metrics: { total: 0 }, percentages: {} };
+      this.info.bChartData = {};
 
       let nameProvided = !(this.name === "");
       if (this.selection === "") {
-        this.info.pChartData = null;
-        this.info.bChartData = null;
+        this.info.pChartData = { metrics: { total: 0 }, percentages: {} };
+        this.info.bChartData = {};
         this.errorMsg =
           "Please specify if you want to search for a user or an organisation";
         this.error = true;
@@ -87,22 +88,21 @@ export default {
           if (this.selection === "organisation") this.getOrgInfo(this.name);
           else this.getUserInfo(this.name);
         } else {
-          this.info.pChartData = null;
-          this.info.bChartData = null;
+          this.info.pChartData = { metrics: { total: 0 }, percentages: {} };
+          this.info.bChartData = {};
           this.errorMsg = "No  " + this.selection + " name provided";
           this.error = true;
         }
       }
     },
     getUserInfo: function (user) {
-      this.info.bChartData = { test: 1 };
+      this.info.pChartData = { metrics: { total: 0 }, percentages: {} };
+      this.info.bChartData = {};
       axios
         .get("https://api.github.com/users/" + user, this.header)
         .then((response) => this.getRepoPages(response))
         .catch((error) => {
           if (error.response) {
-            this.info.pChartData = null;
-            this.info.bChartData = null;
             this.errorMsg = "Error - no user data found";
             this.error = true;
             this.info = "";
@@ -111,17 +111,15 @@ export default {
     },
 
     getOrgInfo: function (org) {
+      this.info.pChartData = { metrics: { total: 0 }, percentages: {} };
       this.info.bChartData = {};
       axios
         .get("https://api.github.com/orgs/" + org, this.header)
         .then((response) => this.getRepoPages(response))
         .catch((error) => {
           if (error.response) {
-            this.info.pChartData = null;
-            this.info.bChartData = null;
             this.errorMsg = "Error - no org data found";
             this.error = true;
-            console.log(this.info);
           } else this.error = false;
         });
     },
@@ -131,7 +129,6 @@ export default {
       console.log("Num of repos: " + numOfRepos);
       // Num of "pages" (containing repo info) we will receive from API
       const pageCount = Math.ceil(numOfRepos / 100); // 100 repos per page response
-      console.log("Num of pages: " + pageCount);
       // Url to repo infor
       const reposUrl = response.data.repos_url;
 
@@ -142,89 +139,70 @@ export default {
           .get(reposUrl + "?page=" + i + "&per_page=100", this.header)
           // Parse page info
           .then((pageReceived) => {
-            this.parsePageData(pageReceived.data);
-          })
-          .then(console.log("Page done: " + i));
+            this.parseData(pageReceived.data);
+          });
       }
     },
-    parsePageData: function (page) {
-      // Have to create tmpData object, can't directly change bChartData
-
+    parseData: async function (page) {
       for (const repo in page) {
         const currentRepo = page[repo];
-        const mainLang = currentRepo.language;
-        let langID = '"' + mainLang + '"';
-        if (langID === null) langID = "other";
-        // check if language already "on record"
-        if (langID in this.info.bChartData) {
-          // just update value
-          this.info.bChartData[langID]++;
-        } else {
-          // add this language to records
-          Vue.set(this.info.bChartData, langID, 1);
-        }
+        const langsUrl = currentRepo.languages_url;
+        // Get lang data for this repo
+        await axios
+          .get(langsUrl, this.header)
+          // Then parse the data
+          .then((repoLangs) => {
+            const langs = Object.keys(repoLangs.data);
+            // continue if there are viable langs in the repo
+            if (langs.length != 0) {
+              /* Parse Bar Chart Info */
+              // main language is always first
+              let mainLang = langs[0];
+              // construct main lang identifier
+              let langID = '"' + mainLang + '"';
+              // change "null" to other
+              if (mainLang === null) langID = '"Other"';
+              // check if language already "on record"
+              if (langID in this.info.bChartData) {
+                // just update value
+                this.info.bChartData[langID]++;
+              } else {
+                // add this language to records
+                Vue.set(this.info.bChartData, langID, 1);
+              }
+
+              /* Parse Pie Chart Info */
+              // get new metrics
+              for (const lang in repoLangs.data) {
+                // construct identifier for lang
+                langID = '"' + lang + '"';
+                // obtain metric value from api
+                const metric = repoLangs.data[lang];
+                // add to total
+                this.info.pChartData.metrics.total += metric;
+                // if lang already "on record"
+                if (langID in this.info.pChartData.metrics) {
+                  // just update value
+                  this.info.pChartData.metrics[langID] += metric;
+                } else {
+                  // add lang to records
+                  Vue.set(this.info.pChartData.metrics, langID, metric);
+                  Vue.set(this.info.pChartData.percentages, langID, 0);
+                }
+              }
+              // re-do percentages with new metrics
+              for (const lang in this.info.pChartData.percentages) {
+                // calculate percentages
+                let percentage = this.info.pChartData.metrics[lang];
+                percentage /= this.info.pChartData.metrics.total;
+                percentage = (percentage * 100).toFixed(2);
+
+                Vue.set(this.info.pChartData.percentages, lang, percentage);
+              }
+            }
+          });
       }
-      console.log(this.info.bChartData);
     },
-    // parseRepoData: async function (reposUrl, numOfRepos) {
-    //   let langUrls = [];
-    //   let reposUsingLangL = {};
-    //   let repoPages = [];
-
-    //   for (const page in repoPages) {
-    //     for (const repo in repoPages[page]) {
-    //       // get main language of repo
-    //       let mainLang = repoPages[page][repo].language;
-
-    //       // create object member identifier
-    //       let identifier = '"' + mainLang + '"';
-
-    //       // check if language is a member of object
-    //       if (identifier in reposUsingLangL) {
-    //         // if so, just increment number of times this language was main language in a repo
-    //         reposUsingLangL[identifier].timesUsed++;
-    //       } else {
-    //         // else, init member
-    //         reposUsingLangL[identifier] = {};
-    //         reposUsingLangL[identifier]["name"] = {}; // have to "init" it to something, so...
-    //         reposUsingLangL[identifier]["name"] = mainLang;
-    //         reposUsingLangL[identifier]["timesUsed"] = 1;
-    //       }
-
-    //       // add current repos languages to list to analyse later
-    //       let langUrl = repoPages[page][repo].languages_url;
-    //       langUrls.push(langUrl);
-    //     }
-    //   }
-
-    //   let langMetrics = {};
-    //   for (const langUrl in langUrls) {
-    //     await axios
-    //       .get(langUrls[langUrl] + "", this.header)
-    //       .then((response) => {
-    //         let langs = Object.keys(response.data);
-    //         for (const lang in langs) {
-    //           if (langs[lang] in langMetrics) {
-    //             langMetrics[langs[lang]] =
-    //               langMetrics[langs[lang]] + response.data[langs[lang]];
-    //           } else {
-    //             langMetrics[langs[lang]] = response.data[langs[lang]];
-    //           }
-    //         }
-    //       });
-    //   }
-
-    //   let totalMetric = 0;
-    //   for (const metric in langMetrics)
-    //     totalMetric = totalMetric + langMetrics[metric];
-
-    //   for (const metric in langMetrics) {
-    //     langMetrics[metric] = (langMetrics[metric] / totalMetric) * 100;
-    //     langMetrics[metric] = langMetrics[metric].toFixed(2); // generate percentages
-    //   }
-
-    //   this.formatData(reposUsingLangL, langMetrics);
-    // },
   },
 };
 </script>
